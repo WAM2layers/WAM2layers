cad1f4c93fbd1e511f68e8e7cddcc626719a03d9
Peter Kalverla <peter.kalverla@gmx.com>
2022-03-25 11:15:01 +0100

    Unix style line endings, remove trailing whitespace and #%% comment symbols

--- Hor_Fluxes_Output_mat.py
+++ Hor_Fluxes_Output_mat.py
@@ -15,7 +15,7 @@ Created on Mon Feb 18 15:30:43 2019
 # We have implemented a datelist function so the model can run for multiple years without having problems with leap years
 
 
-#%% Import libraries
+# Import libraries
 
 import numpy as np
 import scipy.io as sio
@@ -28,7 +28,7 @@ from datetime import timedelta
 import datetime as dt
 
 # to create datelist
-def get_times_daily(startdate, enddate): 
+def get_times_daily(startdate, enddate):
     """ generate a dictionary with date/times"""
     numdays = enddate - startdate
     dateList = []
@@ -36,7 +36,7 @@ def get_times_daily(startdate, enddate):
         dateList.append(startdate + dt.timedelta(days = x))
     return dateList
 
-#%%BEGIN OF INPUT (FILL THIS IN)
+#BEGIN OF INPUT (FILL THIS IN)
 months_length_leap = [31,29,31,30,31,30,31,31,30,31,30,31]
 months_length_nonleap = [31,28,31,30,31,30,31,31,30,31,30,31]
 years = np.arange(2002,2007) #fill in the years # If I fill in more than one year than I need to set the months to 12
@@ -44,15 +44,15 @@ years = np.arange(2002,2007) #fill in the years # If I fill in more than one yea
 
 # Manage the extent of your dataset (FILL THIS IN)
 # Define the latitude and longitude cell numbers to consider and corresponding lakes that should be considered part of the land
-latnrs = np.arange(0,267) # minimal domain 
-lonnrs = np.arange(0,444) 
+latnrs = np.arange(0,267) # minimal domain
+lonnrs = np.arange(0,444)
 
 isglobal = 0 # fill in 1 for global computations (i.e. Earth round), fill in 0 for a local domain with boundaries
 
 daily = 1 # 1 for writing out daily data, 0 for only monthly data
 
 #END OF INPUT
-#%% Datapaths (FILL THIS IN)
+# Datapaths (FILL THIS IN)
 
 lsm_data_ECEarth_T799 = 'landseamask_ECearth_T799.nc' # insert landseamask
 
@@ -62,13 +62,13 @@ output_folder = 'Output_ECEarth/PresentMember4_correct' # insert output folder h
 
 def data_path(y,month,a,years):
     load_fluxes_and_storages = os.path.join(interdata_folder, str(y) + '-' + str(month).zfill(2) + '-' + str(a).zfill(2) + 'fluxes_storages.mat')
-    
-    save_path = os.path.join(output_folder, 'Hor_Fluxes_full-' + str(years[0]) + '-' + str(years[-1]))    
+
+    save_path = os.path.join(output_folder, 'Hor_Fluxes_full-' + str(years[0]) + '-' + str(years[-1]))
     save_path_daily = os.path.join(output_folder, 'Hor_Fluxes_daily_full-' + str(y))
-                                    
+
     return load_fluxes_and_storages,save_path,save_path_daily
 
-#%% Runtime % Results
+# Runtime % Results
 
 start1 = timer()
 
@@ -87,7 +87,7 @@ Fa_Vert_per_year_per_month = np.zeros((len(years),12,len(latitude),len(longitude
 for year in years[:]:
     start = timer()
 
-    if year != 2006: # if no leap year # specific for my dataset as 2006 is a leap year 
+    if year != 2006: # if no leap year # specific for my dataset as 2006 is a leap year
         datelist = get_times_daily(dt.date(year,1,1), dt.date(year,12, 31))
     else: # year == 2006 # if leap year
         datelist = get_times_daily(dt.date(year,1,1), dt.date(year,12, 30))
@@ -102,18 +102,18 @@ for year in years[:]:
     Fa_Vert_per_day = np.zeros((365+ly,len(latitude),len(longitude)))
 
     for i,date in enumerate(datelist[:]):
-             
+
         a=date.day
         yearnumber = date.year
         monthnumber = date.month
         print i, yearnumber, monthnumber, a
-                
+
         datapath = data_path(yearnumber,monthnumber, a,years)
-        
+
         if i > final_time: # a = 365 and not a leapyear
             pass
         else:
-    
+
             # load horizontal fluxes
             loading_FS = sio.loadmat(datapath[0],verify_compressed_data_integrity=False)
             Fa_E_top = loading_FS['Fa_E_top']
@@ -121,42 +121,42 @@ for year in years[:]:
             Fa_E_down = loading_FS['Fa_E_down']
             Fa_N_down = loading_FS['Fa_N_down']
             Fa_Vert = loading_FS['Fa_Vert']
-                        
+
             # save per day
             Fa_E_down_per_day[i,:,:] = np.sum(Fa_E_down, axis =0)
             Fa_E_top_per_day[i,:,:] = np.sum(Fa_E_top, axis =0)
             Fa_N_down_per_day[i,:,:] = np.sum(Fa_N_down, axis =0)
             Fa_N_top_per_day[i,:,:] = np.sum(Fa_N_top, axis =0)
             Fa_Vert_per_day[i,:,:] = np.sum(Fa_Vert, axis =0)
-            
+
             # timer
             end = timer()
             print 'Runtime output for day ' + str(a) + 'in month ' + str(monthnumber) + ' in year ' + str(yearnumber) + ' is',(end - start),' seconds.'
-        
+
     # save daily fluxes on disk
-    if daily == 1:            
+    if daily == 1:
         np.savez_compressed(datapath[2],
                      Fa_E_down_per_day = Fa_E_down_per_day, Fa_E_top_per_day = Fa_E_top_per_day,
-                     Fa_N_down_per_day = Fa_N_down_per_day, Fa_N_top_per_day = Fa_N_top_per_day, 
-                     Fa_Vert_per_day = Fa_Vert_per_day)#    
-    
+                     Fa_N_down_per_day = Fa_N_down_per_day, Fa_N_top_per_day = Fa_N_top_per_day,
+                     Fa_Vert_per_day = Fa_Vert_per_day)#
+
     for m in range(12):
         first_day = int(datetime.date(year,m+1,1).strftime("%j"))
         last_day = int(datetime.date(year,m+1,calendar.monthrange(year,m+1)[1]).strftime("%j"))
         days = np.arange(first_day,last_day+1)-1 # -1 because Python is zero-based
         print year, first_day,last_day,days, startyear
-       
+
         Fa_E_down_per_year_per_month[year-startyear,m,:,:] = (np.squeeze(np.sum(Fa_E_down_per_day[days,:,:], axis = 0)))
         Fa_E_top_per_year_per_month[year-startyear,m,:,:] = (np.squeeze(np.sum(Fa_E_top_per_day[days,:,:], axis = 0)))
         Fa_N_down_per_year_per_month[year-startyear,m,:,:] = (np.squeeze(np.sum(Fa_N_down_per_day[days,:,:], axis = 0)))
         Fa_N_top_per_year_per_month[year-startyear,m,:,:] = (np.squeeze(np.sum(Fa_N_top_per_day[days,:,:], axis = 0)))
         Fa_Vert_per_year_per_month[year-startyear,m,:,:] = (np.squeeze(np.sum(Fa_Vert_per_day[days,:,:], axis = 0)))
 
-# save monthly fluxes on disk                         
-np.savez_compressed(datapath[1], 
-            Fa_E_down_per_year_per_month = Fa_E_down_per_year_per_month, Fa_E_top_per_year_per_month = Fa_E_top_per_year_per_month, 
-            Fa_N_down_per_year_per_month = Fa_N_down_per_year_per_month, Fa_N_top_per_year_per_month = Fa_N_top_per_year_per_month, 
+# save monthly fluxes on disk
+np.savez_compressed(datapath[1],
+            Fa_E_down_per_year_per_month = Fa_E_down_per_year_per_month, Fa_E_top_per_year_per_month = Fa_E_top_per_year_per_month,
+            Fa_N_down_per_year_per_month = Fa_N_down_per_year_per_month, Fa_N_top_per_year_per_month = Fa_N_top_per_year_per_month,
             Fa_Vert_per_year_per_month = Fa_Vert_per_year_per_month)
-    
+
 end1 = timer()
 print 'The total runtime is',(end1-start1),' seconds.'
--- getconstants_pressure_ECEarth.py
+++ getconstants_pressure_ECEarth.py
@@ -14,8 +14,8 @@ Created on Mon Feb 18 15:30:43 2019
 import numpy as np
 from netCDF4 import Dataset
 
-def getconstants_pressure_ECEarth(latnrs,lonnrs,invariant_data): # def getconstants in Python is the same as function in MATLAB. 
-    
+def getconstants_pressure_ECEarth(latnrs,lonnrs,invariant_data): # def getconstants in Python is the same as function in MATLAB.
+
     # load the latitude and longitude from the invariants file
     latitude = Dataset(invariant_data, mode = 'r').variables['LAT'][444:711][::-1] # [degrees north]
     longitude = Dataset(invariant_data, mode = 'r').variables['XAS'][934:1378][::-1] # [degrees east]
@@ -28,26 +28,26 @@ def getconstants_pressure_ECEarth(latnrs,lonnrs,invariant_data): # def getconsta
 
 #    lsm[0,:] = 0 # the northern boundary is always oceanic = 0
 #    lsm[-1,:] = 0 # the southern boundary is always oceanic = 0
-    
-    # Constants 
+
+    # Constants
     g = 9.80665 # [m/s2] from ERA-interim archive
     density_water = 1000 # [kg/m3]
     dg = 111089.56 # [m] length of 1 degree latitude
     timestep = 6*3600 # [s] timestep in the ERA-interim archive (watch out! P & E have 3 hour timestep)
     Erad = 6.371e6 # [m] Earth radius
-    
+
     # Semiconstants
     gridcell = np.abs(longitude[1] - longitude[0]) # [degrees] grid cell size
-    
+
     # new area size calculation:
     lat_n_bound = np.minimum(90.0 , latitude + 0.5*gridcell)
     lat_s_bound = np.maximum(-90.0 , latitude - 0.5*gridcell)
-    
+
     A_gridcell = np.zeros([len(latitude),1])
     A_gridcell[:,0] = (np.pi/180.0)*Erad**2 * abs( np.sin(lat_s_bound*np.pi/180.0) - np.sin(lat_n_bound*np.pi/180.0) ) * gridcell
-  
+
     L_N_gridcell = gridcell * np.cos((latitude + gridcell / 2.0) * np.pi / 180.0) * dg # [m] length northern boundary of a cell
     L_S_gridcell = gridcell * np.cos((latitude - gridcell / 2.0) * np.pi / 180.0) * dg # [m] length southern boundary of a cell
-    L_EW_gridcell = gridcell * dg # [m] length eastern/western boundary of a cell 
-    
+    L_EW_gridcell = gridcell * dg # [m] length eastern/western boundary of a cell
+
     return latitude , longitude , lsm , g , density_water , timestep , A_gridcell , L_N_gridcell , L_S_gridcell , L_EW_gridcell , gridcell
