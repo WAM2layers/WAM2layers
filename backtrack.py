345f3c8ee558c0badd220486f070e68e556c895f
Peter Kalverla <peter.kalverla@gmx.com>
2022-08-17 16:54:20 +0200

    Apply suggestions from code review
    Co-authored-by: Imme1992 <imme.benedict@wur.nl>

--- backtrack.py
+++ backtrack.py
@@ -276,8 +276,7 @@ def backtrack(
 
     # Sa calculation backward in time
     for t in reversed(range(ntime)):
-        # die allerlaatste stap -1 wil je toch niet?????
-        P_region = region * precip[t]  # WHY t-1 ?
+        P_region = region * precip[t] 
         s_total = s_upper[t+1] + s_lower[t+1]
 
         # separate the direction of the vertical flux and make it absolute
