20e25bd494d5d853f2e38788925e19db6438a56c
Peter Kalverla <peter.kalverla@gmx.com>
2022-08-18 11:40:36 +0200

    Apply black formatting

--- preprocess_era5.py
+++ preprocess_era5.py
@@ -37,28 +37,30 @@ datelist = pd.date_range(
     inclusive="left",
 )
 
-def calc_q2m(d2m,ps):
-    '''
+
+def calc_q2m(d2m, ps):
+    """
     Calculate the specific humidity from (surface) pressure and
     dew point temperature
 
     See further details at eq. 7.4 and 7.5 (Page 102) of:
     https://www.ecmwf.int/en/elibrary/20198-ifs-documentation-cy47r3-part-iv-physical-processes
-    '''
-    R_d =287.0597
-    R_v=461.5250
-    a1=611.21
-    a3=17.502
-    a4=32.19
-    T0=273.15
+    """
+    R_d = 287.0597
+    R_v = 461.5250
+    a1 = 611.21
+    a3 = 17.502
+    a4 = 32.19
+    T0 = 273.15
 
-    #Calculation of E saturation water vapour pressure from Teten's formula
-    E=a1*np.exp(a3*(d2m-T0)/(d2m-a4))
+    # Calculation of E saturation water vapour pressure from Teten's formula
+    E = a1 * np.exp(a3 * (d2m - T0) / (d2m - a4))
 
-    #Specific humidity
-    q2m=(R_d/R_v)*E/(ps-((1-R_d/R_v)*E))
+    # Specific humidity
+    q2m = (R_d / R_v) * E / (ps - ((1 - R_d / R_v) * E))
 
     return q2m
+
 
 for date in datelist[:]:
     print(date)
@@ -72,11 +74,11 @@ for date in datelist[:]:
     cp = load_data("cp", date)  # convective precipitation in m (accumulated hourly)
     lsp = load_data("lsp", date)  # large scale precipitation in m (accumulated hourly)
     precip = cp + lsp
-    tcw = load_data("tcw", date) # kg/m2
-    d2m = load_data("d2m", date) # Dew point in K
-    q_surf = calc_q2m(d2m,sp) # kg kg-1
-    u_surf = load_data("u10",date) # in m/s
-    v_surf = load_data("v10",date) # in m/s
+    tcw = load_data("tcw", date)  # kg/m2
+    d2m = load_data("d2m", date)  # Dew point in K
+    q_surf = calc_q2m(d2m, sp)  # kg kg-1
+    u_surf = load_data("u10", date)  # in m/s
+    v_surf = load_data("v10", date)  # in m/s
 
     # Get grid info
     time = u.time.values
@@ -94,7 +96,7 @@ for date in datelist[:]:
     evap = np.abs(np.minimum(evap, 0))
 
     # Create full pressure array (including top of atmosphere and surface pressure, 1100 is a dummy value)
-    levels = np.append(0, np.append(np.array(q.level), [1100, 1100])) * 100 # Pa
+    levels = np.append(0, np.append(np.array(q.level), [1100, 1100])) * 100  # Pa
     levels_reshaped = np.reshape(levels, [1, len(levels), 1, 1])
     p_temp = np.tile(levels_reshaped, (len(time), 1, len(lat), len(lon)))
     sp_reshaped = np.reshape(np.array(sp), [len(time), 1, len(lat), len(lon)])
@@ -106,63 +108,111 @@ for date in datelist[:]:
 
     # find the location of the boundary
     p_boundary = 0.72878581 * np.array(sp) + 7438.803223
-    p_boundary_reshaped = np.reshape(np.array(p_boundary), [len(time), 1, len(lat), len(lon)])
+    p_boundary_reshaped = np.reshape(
+        np.array(p_boundary), [len(time), 1, len(lat), len(lon)]
+    )
     p_boundary_temp = np.tile(p_boundary_reshaped, (1, len(levels), 1, 1))
-    above_boundary = (p_temp < p_boundary_temp)
+    above_boundary = p_temp < p_boundary_temp
     boundary_loc = np.sum(above_boundary, axis=1)
 
     # Create pressure fields: [time, top-upper_levels-boundary-lower_levels-surface:, latitude, longitude]
     # TODO: the loop below works, but is super slow, check if this loop is necessary (if removed possibly keep stored for debugging)
     p_full = np.zeros(p_temp.shape)
-    q_full = np.zeros(p_temp.shape) # assume for p=0, q=0
+    q_full = np.zeros(p_temp.shape)  # assume for p=0, q=0
     u_full = np.zeros(p_temp.shape)
-    u_full[:,0,:,:] = u[:,0,:,:] # assume for p=0, u is equal to u at highest level in the atmosphere
+    u_full[:, 0, :, :] = u[
+        :, 0, :, :
+    ]  # assume for p=0, u is equal to u at highest level in the atmosphere
     v_full = np.zeros(p_temp.shape)
-    v_full[:,0,:,:] = v[:,0,:,:] # assume for p=0, u is equal to u at highest level in the atmosphere
+    v_full[:, 0, :, :] = v[
+        :, 0, :, :
+    ]  # assume for p=0, u is equal to u at highest level in the atmosphere
     for t in range(len(time)):
         for i in range(len(lat)):
             for j in range(len(lon)):
-                p_full[t,1:boundary_loc[t,i,j],i,j] = p_temp[t,1:boundary_loc[t,i,j],i,j]
-                p_full[t,boundary_loc[t,i,j],i,j] = p_boundary[t,i,j]
-                p_full[t,boundary_loc[t,i,j]+1:surf_loc[t,i,j]+1,i,j] = p_temp[t,boundary_loc[t,i,j]:surf_loc[t,i,j],i,j]
-                p_full[t,surf_loc[t,i,j]+1:,i,j] = sp[t,i,j]
+                p_full[t, 1 : boundary_loc[t, i, j], i, j] = p_temp[
+                    t, 1 : boundary_loc[t, i, j], i, j
+                ]
+                p_full[t, boundary_loc[t, i, j], i, j] = p_boundary[t, i, j]
+                p_full[
+                    t, boundary_loc[t, i, j] + 1 : surf_loc[t, i, j] + 1, i, j
+                ] = p_temp[t, boundary_loc[t, i, j] : surf_loc[t, i, j], i, j]
+                p_full[t, surf_loc[t, i, j] + 1 :, i, j] = sp[t, i, j]
 
-                q_full[t,1:boundary_loc[t,i,j],i,j] = q[t,0:boundary_loc[t,i,j]-1,i,j]
-                q_full[t,boundary_loc[t,i,j],i,j] = q[t,boundary_loc[t,i,j]-2,i,j] + \
-                                                    ( (q[t,boundary_loc[t,i,j]-1,i,j] - q[t,boundary_loc[t,i,j]-2,i,j]) \
-                                                     * (p_boundary[t,i,j] - p_full[t,boundary_loc[t,i,j]-1,i,j]) ) \
-                                                        / (p_full[t,boundary_loc[t,i,j]+1,i,j] - p_full[t,boundary_loc[t,i,j]-1,i,j])
-                q_full[t,boundary_loc[t,i,j]+1:surf_loc[t,i,j]+1,i,j] = q[t,boundary_loc[t,i,j]-1:surf_loc[t,i,j]-1,i,j]
-                q_full[t,surf_loc[t,i,j]:,i,j] = q_surf[t,i,j]
+                q_full[t, 1 : boundary_loc[t, i, j], i, j] = q[
+                    t, 0 : boundary_loc[t, i, j] - 1, i, j
+                ]
+                q_full[t, boundary_loc[t, i, j], i, j] = q[
+                    t, boundary_loc[t, i, j] - 2, i, j
+                ] + (
+                    (
+                        q[t, boundary_loc[t, i, j] - 1, i, j]
+                        - q[t, boundary_loc[t, i, j] - 2, i, j]
+                    )
+                    * (p_boundary[t, i, j] - p_full[t, boundary_loc[t, i, j] - 1, i, j])
+                ) / (
+                    p_full[t, boundary_loc[t, i, j] + 1, i, j]
+                    - p_full[t, boundary_loc[t, i, j] - 1, i, j]
+                )
+                q_full[t, boundary_loc[t, i, j] + 1 : surf_loc[t, i, j] + 1, i, j] = q[
+                    t, boundary_loc[t, i, j] - 1 : surf_loc[t, i, j] - 1, i, j
+                ]
+                q_full[t, surf_loc[t, i, j] :, i, j] = q_surf[t, i, j]
 
-                u_full[t,1:boundary_loc[t,i,j],i,j] = u[t,0:boundary_loc[t,i,j]-1,i,j]
-                u_full[t,boundary_loc[t,i,j],i,j] = u[t,boundary_loc[t,i,j]-2,i,j] + \
-                                                    ( (u[t,boundary_loc[t,i,j]-1,i,j] - u[t,boundary_loc[t,i,j]-2,i,j]) \
-                                                     * (p_boundary[t,i,j] - p_full[t,boundary_loc[t,i,j]-1,i,j]) ) \
-                                                        / (p_full[t,boundary_loc[t,i,j]+1,i,j] - p_full[t,boundary_loc[t,i,j]-1,i,j])
-                u_full[t,boundary_loc[t,i,j]+1:surf_loc[t,i,j]+1,i,j] = u[t,boundary_loc[t,i,j]-1:surf_loc[t,i,j]-1,i,j]
-                u_full[t,surf_loc[t,i,j]:,i,j] = u_surf[t,i,j]
+                u_full[t, 1 : boundary_loc[t, i, j], i, j] = u[
+                    t, 0 : boundary_loc[t, i, j] - 1, i, j
+                ]
+                u_full[t, boundary_loc[t, i, j], i, j] = u[
+                    t, boundary_loc[t, i, j] - 2, i, j
+                ] + (
+                    (
+                        u[t, boundary_loc[t, i, j] - 1, i, j]
+                        - u[t, boundary_loc[t, i, j] - 2, i, j]
+                    )
+                    * (p_boundary[t, i, j] - p_full[t, boundary_loc[t, i, j] - 1, i, j])
+                ) / (
+                    p_full[t, boundary_loc[t, i, j] + 1, i, j]
+                    - p_full[t, boundary_loc[t, i, j] - 1, i, j]
+                )
+                u_full[t, boundary_loc[t, i, j] + 1 : surf_loc[t, i, j] + 1, i, j] = u[
+                    t, boundary_loc[t, i, j] - 1 : surf_loc[t, i, j] - 1, i, j
+                ]
+                u_full[t, surf_loc[t, i, j] :, i, j] = u_surf[t, i, j]
 
-                v_full[t,1:boundary_loc[t,i,j],i,j] = v[t,0:boundary_loc[t,i,j]-1,i,j]
-                v_full[t,boundary_loc[t,i,j],i,j] = v[t,boundary_loc[t,i,j]-2,i,j] + \
-                                                    ( (v[t,boundary_loc[t,i,j]-1,i,j] - v[t,boundary_loc[t,i,j]-2,i,j]) \
-                                                     * (p_boundary[t,i,j] - p_full[t,boundary_loc[t,i,j]-1,i,j]) ) \
-                                                        / (p_full[t,boundary_loc[t,i,j]+1,i,j] - p_full[t,boundary_loc[t,i,j]-1,i,j])
-                v_full[t,boundary_loc[t,i,j]+1:surf_loc[t,i,j]+1,i,j] = v[t,boundary_loc[t,i,j]-1:surf_loc[t,i,j]-1,i,j]
-                v_full[t,surf_loc[t,i,j]:,i,j] = v_surf[t,i,j]
+                v_full[t, 1 : boundary_loc[t, i, j], i, j] = v[
+                    t, 0 : boundary_loc[t, i, j] - 1, i, j
+                ]
+                v_full[t, boundary_loc[t, i, j], i, j] = v[
+                    t, boundary_loc[t, i, j] - 2, i, j
+                ] + (
+                    (
+                        v[t, boundary_loc[t, i, j] - 1, i, j]
+                        - v[t, boundary_loc[t, i, j] - 2, i, j]
+                    )
+                    * (p_boundary[t, i, j] - p_full[t, boundary_loc[t, i, j] - 1, i, j])
+                ) / (
+                    p_full[t, boundary_loc[t, i, j] + 1, i, j]
+                    - p_full[t, boundary_loc[t, i, j] - 1, i, j]
+                )
+                v_full[t, boundary_loc[t, i, j] + 1 : surf_loc[t, i, j] + 1, i, j] = v[
+                    t, boundary_loc[t, i, j] - 1 : surf_loc[t, i, j] - 1, i, j
+                ]
+                v_full[t, surf_loc[t, i, j] :, i, j] = v_surf[t, i, j]
 
                 # TODO, if we keep this code like this, create a function for the manipulation of q, u, and v (uses the same code)
 
     # TODO: add a quick check whether the new pressure level list is strictly increasing, otherwise give an error!
 
     # Interpolate to new levels
-    midpoints = 0.5 * (p_full[:,:-1,:,:] + p_full[:,1:,:,:]) # TODO: not used, but could be to used when converted to x_array
-    q_mid = 0.5 * (q_full[:,:-1,:,:] + q_full[:,1:,:,:])
-    u_mid = 0.5 * (u_full[:,:-1,:,:] + u_full[:,1:,:,:])
-    v_mid = 0.5 * (v_full[:,:-1,:,:] + v_full[:,1:,:,:])
+    midpoints = 0.5 * (
+        p_full[:, :-1, :, :] + p_full[:, 1:, :, :]
+    )  # TODO: not used, but could be to used when converted to x_array
+    q_mid = 0.5 * (q_full[:, :-1, :, :] + q_full[:, 1:, :, :])
+    u_mid = 0.5 * (u_full[:, :-1, :, :] + u_full[:, 1:, :, :])
+    v_mid = 0.5 * (v_full[:, :-1, :, :] + v_full[:, 1:, :, :])
 
     # Calculate pressure jump
-    dp = p_full[:,1:,:,:] - p_full[:,:-1,:,:]
+    dp = p_full[:, 1:, :, :] - p_full[:, :-1, :, :]
 
     # Determine the fluxes and states
     fx = u_mid * q_mid * dp / g  # eastward atmospheric moisture flux (kg*m-1*s-1)
@@ -172,15 +222,19 @@ for date in datelist[:]:
     # convert fa_e, fa_n and cwv to xarray # TODO: move to preprocessing function
     def fs_to_xarray(fs, dimension_data, mid_levels, delta_pressure):
         fs_xr = xr.DataArray(
-            data = fs,
-            dims = ["time", "level", "latitude", "longitude"],
-            coords = dict(
-                time = dimension_data.time,
-                latitude = dimension_data.latitude,
-                longitude = dimension_data.longitude,
-                mid_level = (["time", "level", "latitude", "longitude"], mid_levels),
-                delta_pressure = (["time", "level", "latitude", "longitude"], delta_pressure)
-            ) )
+            data=fs,
+            dims=["time", "level", "latitude", "longitude"],
+            coords=dict(
+                time=dimension_data.time,
+                latitude=dimension_data.latitude,
+                longitude=dimension_data.longitude,
+                mid_level=(["time", "level", "latitude", "longitude"], mid_levels),
+                delta_pressure=(
+                    ["time", "level", "latitude", "longitude"],
+                    delta_pressure,
+                ),
+            ),
+        )
         return fs_xr
 
     fx = fs_to_xarray(fa_e, q, midpoints, dp)
@@ -188,7 +242,7 @@ for date in datelist[:]:
     cwv = fs_to_xarray(cwv, q, midpoints, dp)
 
     # Integrate fluxes and state
-    upper_layer = above_boundary[:,:-1,:,:]
+    upper_layer = above_boundary[:, :-1, :, :]
     lower_layer = ~upper_layer
 
     fx_lower = fx.where(lower_layer).sum(dim="level")  # kg*m-1*s-1
