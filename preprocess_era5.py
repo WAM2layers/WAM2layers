efb7e24ce02596a9ae0f2842b7391298023e8336
Ruud van der Ent <r.j.vanderent@tudelft.nl>
2022-08-18 15:23:04 +0200

    Calculate q2m and q,u,v, interpolation between top-boundary-surface (#100)
    * Read surface data and calculate 2m q
    
    * combined calculate_q2m and dp_calc
    
    * Add minimal example avoiding loops (#103)
    
    * Align with model-levels (fvert, redefine etc to tracking)
    
    * Apply black formatting
    
    * refactor insertion of additional pressure level and make sure the code runs
    
    * Move humidity calculation to utilities
    
    * Clean up test code
    
    * reorganize a bit
    
    Co-authored-by: Chris weijenborg <Chris.weijenborg@wur.nl>
    Co-authored-by: chrisweij <97346631+chrisweij@users.noreply.github.com>
    Co-authored-by: Peter Kalverla <peter.kalverla@gmx.com>

--- preprocess_era5.py
+++ preprocess_era5.py
@@ -1,11 +1,12 @@
+from pathlib import Path
+
 import numpy as np
 import pandas as pd
 import xarray as xr
 import yaml
-from pathlib import Path
 
-from preprocessing import get_grid_info, get_stable_fluxes, get_vertical_transport
-
+from preprocessing import (get_grid_info, insert_level, interpolate,
+                           sortby_ndarray)
 
 # Set constants
 g = 9.80665  # [m/s2]
@@ -40,56 +41,104 @@ datelist = pd.date_range(
 for date in datelist[:]:
     print(date)
 
-    # Load data
+    # 4d fields
+    q = load_data("q", date)  # in kg kg-1
     u = load_data("u", date)  # in m/s
     v = load_data("v", date)  # in m/s
-    q = load_data("q", date)  # in kg kg-1
-    sp = load_data("sp", date)  # in Pa
+
+    # Precipitation and evaporation
     evap = load_data("e", date)  # in m (accumulated hourly)
     cp = load_data("cp", date)  # convective precipitation in m (accumulated hourly)
     lsp = load_data("lsp", date)  # large scale precipitation in m (accumulated hourly)
     precip = cp + lsp
+
+    # TODO: not used
     tcw = load_data("tcw", date)  # kg/m2
 
+    p_surf = load_data("sp", date)  # in Pa
+    d_surf = load_data("d2m", date)  # Dew point in K
+    u_surf = load_data("u10", date)  # in m/s
+    v_surf = load_data("v10", date)  # in m/s
+    q_surf = calculate_humidity(d2m, p_surf)  # kg kg-1
+
     # Get grid info
+    time = u.time.values
     lat = u.latitude.values
     lon = u.longitude.values
-    a_gridcell, l_ew_gridcell, l_mid_gridcell = get_grid_info(lat, lon)
+    a_gridcell, l_ew_gridcell, l_mid_gridcell = get_grid_info(u)
 
     # Calculate volumes
-    evap *= a_gridcell  # m3
-    precip *= a_gridcell  # m3
+    # TODO: shall we do this in the tracking?
+    evap *= a_gridcell[None, :, None]  # m3
+    precip *= a_gridcell[None, :, None]  # m3
 
     # Transfer negative (originally positive) values of evap to precip
     precip = np.maximum(precip, 0) + np.maximum(evap, 0)
     # Change sign convention to all positive,
     evap = np.abs(np.minimum(evap, 0))
 
-    # Create pressure array
-    levels = q.level  # in hPa
-    p = levels.broadcast_like(u) * 100  # Pa
+    # Create pressure array with the same dimensions as u, q, and v
+    p = u.level.broadcast_like(u)
 
-    # Interpolate to new levels
-    edges = 0.5 * (levels.values[1:] + levels.values[:-1])
-    u = u.interp(level=edges)
-    v = v.interp(level=edges)
-    q = q.interp(level=edges)
+    # Insert top of atmosphere values
+    u = insert_level(u, u.isel(level=0), 0)
+    v = insert_level(v, v.isel(level=0), 0)
+    q = insert_level(q, q.isel(level=0), 0)
+    p = insert_level(p, 0, 0)
+
+    # Insert surface level values
+    u = insert_level(u, u_surf, 110000)
+    v = insert_level(v, v_surf, 110000)
+    q = insert_level(q, q_surf, 110000)
+    p = insert_level(p, p_surf, 110000)
+
+    # Sort arrays by pressure (ascending)
+    u.values = sortby_ndarray(u.values, p.values, axis=1)
+    v.values = sortby_ndarray(v.values, p.values, axis=1)
+    q.values = sortby_ndarray(q.values, p.values, axis=1)
+    p.values = sortby_ndarray(p.values, p.values, axis=1)
+
+    # Insert boundary level values (at a ridiculous dummy pressure value)
+    p_boundary = 0.72878581 * np.array(p_surf) + 7438.803223
+    u = insert_level(u, interpolate(p_boundary, p, u), 150000)
+    v = insert_level(v, interpolate(p_boundary, p, v), 150000)
+    q = insert_level(q, interpolate(p_boundary, p, q), 150000)
+    p = insert_level(p, p_boundary, 150000)
+
+    # Sort arrays by pressure once more (ascending)
+    u.values = sortby_ndarray(u.values, p.values, axis=1)
+    v.values = sortby_ndarray(v.values, p.values, axis=1)
+    q.values = sortby_ndarray(q.values, p.values, axis=1)
+    p.values = sortby_ndarray(p.values, p.values, axis=1)
+
+    # Reset level coordinate as its values have become meaningless
+    nlev = u.level.size
+    u = u.assign_coords(level=np.arange(nlev))
+    v = v.assign_coords(level=np.arange(nlev))
+    q = q.assign_coords(level=np.arange(nlev))
+    p = p.assign_coords(level=np.arange(nlev))
 
     # Calculate pressure jump
-    dp = p.diff(dim="level")
-    dp["level"] = edges
+    dp = p.diff("level")
+    assert np.all(dp > 0), "Pressure levels should increase monotonically"
+
+    # Interpolate to midpoints
+    midpoints = 0.5 * (u.level.values[1:] + u.level.values[:-1])
+    u = u.interp(level=midpoints)
+    v = v.interp(level=midpoints)
+    q = q.interp(level=midpoints)
+    p = p.interp(level=midpoints)
+    dp.assign_coords(level=midpoints)
 
     # Determine the fluxes and states
     fx = u * q * dp / g  # eastward atmospheric moisture flux (kg*m-1*s-1)
-    fy = v * q * dp / g  # northward atmospheric moisture flux (#kg*m-1*s-1)
-    cwv = q * dp / g * a_gridcell / density_water  # column water vapor (m3)
-
-    # Split in 2 layers
-    P_boundary = 0.72878581 * sp + 7438.803223
-    lower_layer = (dp.level < sp / 100) & (dp.level > P_boundary / 100)
-    upper_layer = dp.level < P_boundary / 100
+    fy = v * q * dp / g  # northward atmospheric moisture flux (kg*m-1*s-1)
+    cwv = q * dp / g * a_gridcell[None, None, :, None] / density_water  # column water vapor (m3)
 
     # Integrate fluxes and state
+    upper_layer = p < p_boundary[:, None, :, :]
+    lower_layer = ~upper_layer
+
     fx_lower = fx.where(lower_layer).sum(dim="level")  # kg*m-1*s-1
     fy_lower = fy.where(lower_layer).sum(dim="level")  # kg*m-1*s-1
     s_lower = cwv.where(lower_layer).sum(dim="level")  # m3
@@ -98,92 +147,26 @@ for date in datelist[:]:
     fy_upper = fy.where(upper_layer).sum(dim="level")  # kg*m-1*s-1
     s_upper = cwv.where(upper_layer).sum(dim="level")  # m3
 
-    print(
-        "Check calculation water vapor, this value should be zero:",
-        (cwv.sum(dim="level") - (s_upper + s_lower)).sum().values,
-    )
-
-    tcwm3 = tcw * a_gridcell[np.newaxis, :] / density_water  # m3
-
-    # Change units to m3, based on target frequency (not incoming frequency!)
-    target_freq = config["target_frequency"]
-    total_seconds = pd.Timedelta(target_freq).total_seconds()
-    fx_upper *= total_seconds * (l_ew_gridcell / density_water)
-    fx_lower *= total_seconds * (l_ew_gridcell / density_water)
-    fy_upper *= total_seconds * (l_mid_gridcell[None, :, None] / density_water)
-    fy_lower *= total_seconds * (l_mid_gridcell[None, :, None] / density_water)
-
-    # Increased time resolution; states at midpoints, fluxes at the edges
-    old_time = s_upper.time.values
-    newtime_states = pd.date_range(old_time[0], old_time[-1], freq=target_freq)
-    newtime_fluxes = newtime_states[:-1] + pd.Timedelta(target_freq) / 2
-
-    s_upper = s_upper.interp(time=newtime_states).values
-    s_lower = s_lower.interp(time=newtime_states).values
-    fx_upper = fx_upper.interp(time=newtime_fluxes).values
-    fy_upper = fy_upper.interp(time=newtime_fluxes).values
-    fx_lower = fx_lower.interp(time=newtime_fluxes).values
-    fy_lower = fy_lower.interp(time=newtime_fluxes).values
-    precip = (precip.reindex(time=newtime_fluxes, method="bfill") / 4).values
-    evap = (evap.reindex(time=newtime_fluxes, method="bfill") / 4).values
-
-    # Stabilize horizontal fluxes
-    fx_upper, fy_upper = get_stable_fluxes(fx_upper, fy_upper, s_upper)
-    fx_lower, fy_lower = get_stable_fluxes(fx_lower, fy_lower, s_lower)
-
-    # Determine the vertical moisture flux
-    f_vert = get_vertical_transport(
-        fx_upper,
-        fx_lower,
-        fy_upper,
-        fy_lower,
-        evap,
-        precip,
-        s_upper,
-        s_lower,
-        config["periodic_boundary"],
-        config["kvf"],
+    # Check column water vapor conservation
+    np.testing.assert_array_almost_equal(
+        cwv.sum(dim="level"),
+        s_upper + s_lower,
+        err_msg="Column water vapor should be approximately 0"
     )
 
     # Save preprocessed data
-    # Note: fluxes (dim: time) are at the edges of the timesteps,
-    # while states (dim: time2) are at the midpoints and include next midnight
-    # so the first state from day 2 will overlap with the last flux from day 1
     filename = f"{date.strftime('%Y-%m-%d')}_fluxes_storages.nc"
     output_path = output_dir / filename
 
     xr.Dataset(
         {  # TODO: would be nice to add coordinates and units as well
-            "fx_upper": (
-                ["time_fluxes", "lat", "lon"],
-                fx_upper,
-                {"units": "m**3"},
-            ),
-            "fy_upper": (
-                ["time_fluxes", "lat", "lon"],
-                fy_upper,
-                {"units": "m**3"},
-            ),
-            "fx_lower": (
-                ["time_fluxes", "lat", "lon"],
-                fx_lower,
-                {"units": "m**3"},
-            ),
-            "fy_lower": (
-                ["time_fluxes", "lat", "lon"],
-                fy_lower,
-                {"units": "m**3"},
-            ),
-            "s_upper": (["time_states", "lat", "lon"], s_upper, {"units": "m**3"}),
-            "s_lower": (["time_states", "lat", "lon"], s_lower, {"units": "m**3"}),
-            "f_vert": (["time_fluxes", "lat", "lon"], f_vert, {"units": "m**3"}),
-            "evap": (["time_fluxes", "lat", "lon"], evap, {"units": "m**3"}),
-            "precip": (["time_fluxes", "lat", "lon"], precip, {"units": "m**3"}),
-        },
-        coords={
-            "time_fluxes": newtime_fluxes,
-            "time_states": newtime_states,
-            "lat": u.latitude.values,
-            "lon": u.longitude.values,
-        },
+            "fx_upper": fx_upper,
+            "fy_upper": fy_upper,
+            "fx_lower": fx_lower,
+            "fy_lower": fy_lower,
+            "s_upper": s_upper,
+            "s_lower": s_lower,
+            "evap": evap,
+            "precip": precip,
+        }
     ).to_netcdf(output_path)
