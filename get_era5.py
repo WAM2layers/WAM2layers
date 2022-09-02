685e4806a4fad53fb7ebb01f33733de78ae00cae
Chris weijenborg <Chris.weijenborg@wur.nl>
2022-09-02 16:02:05 +0000

    Option to download pressure levels

--- get_era5.py
+++ get_era5.py
@@ -18,6 +18,8 @@ else:
     levlist = s.join(str(e) for e in config['levels'])
     
 timlist  = '00:00:00/01:00:00/02:00:00/03:00:00/04:00:00/05:00:00/06:00:00/07:00:00/08:00:00/09:00:00/10:00:00/11:00:00/12:00:00/13:00:00/14:00:00/15:00:00/16:00:00/17:00:00/18:00:00/19:00:00/20:00:00/21:00:00/22:00:00/23:00:00'
+area = [60, -50,30,30] # North, West, South, East.          Default: global
+grid = [0.25, 0.25]  # Latitude/longitude grid.           Default: 0.25 x 0.25
 
 ######################################################################
 # For model levels the following variables are downloaded:
@@ -37,7 +39,7 @@ if(config['level_type'] == 'model_levels'):
         
         #Loop over model level variables
         for i in range(len(var_names)):
-            outfile = config['input_folder'] + '/ERA5_' + dtstr + "_" + var_names[i] + '_ml_' +'.nc'
+            outfile = config['input_folder'] + '/ERA5_' + dtstr + "_" + var_names[i] + '_ml_.nc'
             
             c.retrieve('reanalysis-era5-complete', {
             'class': 'ea',
@@ -50,66 +52,71 @@ if(config['level_type'] == 'model_levels'):
             'time': timlist,
             'type': 'an',
             'format': 'netcdf',
-             'area': [60, -50,30,30],
-             'grid'          : [0.25, 0.25],       # Latitude/longitude grid.           Default: 0.25 x 0.25
+             'area': area,
+             'grid': grid,       # Latitude/longitude grid.           Default: 0.25 x 0.25
         }, outfile) 
+        
+        #Loop over single level variables
         for i in range(len(sfc_long)):
             outfile = config['input_folder'] + '/ERA5_' + dtstr + "_" + sfc_short[i] + '.nc'  
             c.retrieve(
             'reanalysis-era5-single-levels',
             {
                 'product_type': 'reanalysis',
-                'format': 'netcdf',
                 'variable': sfc_long[i],
                 'date': dtstr,
                 'time': timlist,
-                'area'          : [60, -50, 30, 30], # North, West, South, East.          Default: global
-                'grid'          : [0.25, 0.25],       # Latitude/longitude grid.           Default: 0.25 x 0.25
+                'area': area, 
+                'grid': grid,      
                 'format': 'netcdf',
             },
             outfile)
 ######################################################################
-# For model levels the following variables are downloaded:
-# u,v, q on selected levels
+# For pressure levels the following variables are downloaded:
+# u,v, q on selected pressure levels
 # tp (total precipitation), e (evaporation) and sp (surface pressure)
+# + t2m (2 meter temperature), 
 ######################################################################
-elif(confif.levtype == 'pressure_levels'):
+elif(config['level_type'] == 'pressure_levels'):
     print("Getting pressure levels")
     
-
+    varp= ['specific_humidity', 'u_component_of_wind', 'v_component_of_wind']  
+    varpshort = ['q','u','v']
+    
+    sfc_short = ['tp','e','sp']
+    sfc_long = ['mean_sea_level_pressure', 'evaporation', 'total_precipitation']
+    
+    for date in datelist:
+        #For first day of month, datadir should be created
+        dtstr = date.strftime("%Y-%m-%d")            
+        for i in range(len(varp)):
+            outfile = config['input_folder'] + '/ERA5_' + dtstr + "_" + varpshort[i] + '_pl_.nc'
+            c.retrieve(
+            'reanalysis-era5-pressure-levels',
+            {
+                'product_type': 'reanalysis',
+                'variable': varp[i],
+                'pressure_level': levlist,
+                'date': dtstr,
+                'time': timlist,
+                'area': area, 
+                'grid': grid, 
+                'format': 'netcdf',
+            },
+            outfile)
             
-            
-            
-
-'''
-c.retrieve('reanalysis-era5-complete', {
-            'class': 'ea',
-            'date': '2021-07-01/to/2021-07-15',
-            'expver': '1',
-            'levelist': '1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20/21/22/23/24/25/26/27/28/29/30/31/32/33/34/35/36/37/38/39/40/41/42/43/44/45/46/47/48/49/50/51/52/53/54/55/56/57/58/59/60/61/62/63/64/65/66/67/68/69/70/71/72/73/74/75/76/77/78/79/80/81/82/83/84/85/86/87/88/89/90/91/92/93/94/95/96/97/98/99/100/101/102/103/104/105/106/107/108/109/110/111/112/113/114/115/116/117/118/119/120/121/122/123/124/125/126/127/128/129/130/131/132/133/134/135/136/137',
-            'levtype': 'ml',
-            'param': '132', 
-            'stream': 'oper',
-            'time': '00:00:00/01:00:00/02:00:00/03:00:00/04:00:00/05:00:00/06:00:00/07:00:00/08:00:00/09:00:00/10:00:00/11:00:00/12:00:00/13:00:00/14:00:00/15:00:00/16:00:00/17:00:00/18:00:00/19:00:00/20:00:00/21:00:00/22:00:00/23:00:00',
-            'type': 'an',
-            'format': 'netcdf',
-             'area': [60, -50,30,30],
-             'grid'          : [0.25, 0.25],       # Latitude/longitude grid.           Default: 0.25 x 0.25
-        }, 'ERA5_202107_ml_v.nc')
-
-c.retrieve('reanalysis-era5-complete', {
-            'class': 'ea',
-            'date': '2021-07-01/to/2021-07-15',
-            'expver': '1',
-            'levelist': '1/2/3/4/5/6/7/8/9/10/11/12/13/14/15/16/17/18/19/20/21/22/23/24/25/26/27/28/29/30/31/32/33/34/35/36/37/38/39/40/41/42/43/44/45/46/47/48/49/50/51/52/53/54/55/56/57/58/59/60/61/62/63/64/65/66/67/68/69/70/71/72/73/74/75/76/77/78/79/80/81/82/83/84/85/86/87/88/89/90/91/92/93/94/95/96/97/98/99/100/101/102/103/104/105/106/107/108/109/110/111/112/113/114/115/116/117/118/119/120/121/122/123/124/125/126/127/128/129/130/131/132/133/134/135/136/137',
-            'levtype': 'ml',
-            'param': '133',  #/131/132/133/135',
-            'stream': 'oper',
-            'time': '00:00:00/01:00:00/02:00:00/03:00:00/04:00:00/05:00:00/06:00:00/07:00:00/08:00:00/09:00:00/10:00:00/11:00:00/12:00:00/13:00:00/14:00:00/15:00:00/16:00:00/17:00:00/18:00:00/19:00:00/20:00:00/21:00:00/22:00:00/23:00:00',
-            'type': 'an',
-            'format': 'netcdf',
-             'area': [60, -50,30,30],
-             'grid'          : [0.25, 0.25],       # Latitude/longitude grid.           Default: 0.25 x 0.25
-        }, 'ERA5_202107_ml_q.nc')
-                                    
-'''
+            #Loop over single level variables
+        for i in range(len(sfc_long)):
+            outfile = config['input_folder'] + '/ERA5_' + dtstr + "_" + sfc_short[i] + '.nc'  
+            c.retrieve(
+            'reanalysis-era5-single-levels',
+            {
+                'product_type': 'reanalysis',
+                'variable': sfc_long[i],
+                'date': dtstr,
+                'time': timlist,
+                'area': area,
+                'grid': grid,  
+                'format': 'netcdf',
+            },
+            outfile)
