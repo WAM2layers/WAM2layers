f6bc7fffe855db51acff759722b23fe1c6ec4abf
Peter Kalverla <peter.kalverla@gmx.com>
2022-12-14 15:43:43 +0100

    without slash?

--- docs/conf.py
+++ docs/conf.py
@@ -4,7 +4,7 @@
 # Make sure autodoc can find code
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(0, os.path.abspath('..'))
 
 # load extensions
 extensions = ["myst_nb", "sphinx_rtd_theme", "sphinx.ext.autodoc", "sphinx.ext.napoleon"]
