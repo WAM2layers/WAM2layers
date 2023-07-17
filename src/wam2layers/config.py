37a1bb4706f03d0795605c7df1f6acb5e2e16de5
Peter Kalverla <peter.kalverla@gmx.com>
2023-07-17 16:28:28 +0200

    Also use Optional

--- src/wam2layers/config.py
+++ src/wam2layers/config.py
@@ -2,7 +2,7 @@ from pydantic import field_validator, ConfigDict, BaseModel, FilePath, model_val
 import yaml
 from datetime import datetime
 from pathlib import Path
-from typing import List, Literal, Union
+from typing import List, Literal, Optional, Union
 
 
 class Config(BaseModel):
@@ -277,7 +277,7 @@ class Config(BaseModel):
 
     """
 
-    chunks: "Union[None, dict[str, int]]"
+    chunks: Optional[dict[str, int]]
     """Whether to use dask.
 
     Using dask can help process large datasets without memory issues, but its
