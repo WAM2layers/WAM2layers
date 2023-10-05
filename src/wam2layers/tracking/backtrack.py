8cff79c7e0ca55351e4f05f3e64f2601b7b99c31
Peter Kalverla <peter.kalverla@gmx.com>
2023-10-05 16:05:08 +0200

    more docstrings

--- src/wam2layers/tracking/backtrack.py
+++ src/wam2layers/tracking/backtrack.py
@@ -154,12 +154,16 @@ def pad_boundaries(*args, periodic=False):
 def horizontal_advection(s, u, v, periodic_x=False):
     """Calculate advection on a staggered grid using a donor cell scheme.
 
-    It solves the advection equation `grad(u*s)` where u is the velocity vector
-    and s is any scalar, which is discretized as a double upwind scheme:
+    Boundaries are padded with 0 or periodic boundaries to maintain the original
+    grid size.
+
+    The advection equation reads `grad(u*s)` where u is the velocity vector and
+    s is any scalar. Using i for grid cell indices in the x direction and j for
+    the y-direction (increasing northwards), the donor cell scheme reads:
 
     s(i-1/2) = s(i-1) if u(i-1/2) > 0
                s(i+1) otherwise
-    s(j-1/2) = s(j-1) if v(j-1/2) > 0   TODO: check direction
+    s(j-1/2) = s(j-1) if v(j-1/2) > 0
                s(j+1) otherwise
 
     d(us)/dx = u(i-1/2)*s(i-1/2) - u(i+1/2)*s(i+1/2)
@@ -167,28 +171,43 @@ def horizontal_advection(s, u, v, periodic_x=False):
 
     adv(s) = d(us)/dx + d(vs)/dy
 
-    Boundaries are padded with 0 or periodic boundaries
-    to maintain the original grid size.
-
     Arguments:
-        s: array of shape [M, N]
+        s: array of shape [M, N]. It is assumed that the grid dimensions are
+            [latitude, longitude] and latitude is in decreasing order.
         u: array of shape = [M-2, N-1]
         v: array of shape = [M-1, N-2]
 
     Returns:
         array of shape [M, N]
 
-    Example:
+    Examples:
 
+        Create a simple array:
         >>> s = np.zeros((5, 5))
         >>> s[2, 2] = 1
         >>> u = np.ones((3, 4)) * .5
         >>> v = np.ones((4, 3)) * .5
+        >>> s
+        array([[0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0.],
+               [0., 0., 1., 0., 0.],
+               [0., 0., 0., 0., 0.],
+               [0., 0., 0., 0., 0.]])
+
+        Calculate the advection for a single time step (forward)
         >>> horizontal_advection(s, u, v)
         array([[ 0. ,  0. ,  0. ,  0. ,  0. ],
                [ 0. ,  0. ,  0.5,  0. ,  0. ],
                [ 0. ,  0. , -1. ,  0.5,  0. ],
                [ 0. ,  0. ,  0. ,  0. ,  0. ],
+               [ 0. ,  0. ,  0. ,  0. ,  0. ]])
+
+        Backtracking can be done by reversing the velocities:
+        >>> horizontal_advection(s, -u, -v)
+        array([[ 0. ,  0. ,  0. ,  0. ,  0. ],
+               [ 0. ,  0. ,  0. ,  0. ,  0. ],
+               [ 0. ,  0.5, -1. ,  0. ,  0. ],
+               [ 0. ,  0. ,  0.5,  0. ,  0. ],
                [ 0. ,  0. ,  0. ,  0. ,  0. ]])
 
     """
@@ -198,7 +217,7 @@ def horizontal_advection(s, u, v, periodic_x=False):
 
     west = np.s_[:-1]
     east = np.s_[1:]
-    south = np.s_[1:]  # TODO check grid direction
+    south = np.s_[1:]
     north = np.s_[:-1]
     inner = np.s_[1:-1]
 
