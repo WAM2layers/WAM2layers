cf163ac2e05c3f7c41f65ae237afe0177b536f61
Yang <y.liu@esciencecenter.nl>
2023-10-05 16:31:54 +0200

    add visualize check and micromamba ci

--- tests/test_workflow.py
+++ tests/test_workflow.py
@@ -1,11 +1,19 @@
 """Tests for entire workflows."""
 from pathlib import Path
 
+import matplotlib.testing.compare
 import numpy
 import xarray as xr
 from click.testing import CliRunner
 
 from wam2layers.cli import cli
+
+
+CARTOPY_INSTALLED = True
+try:
+    import cartopy.crs as _
+except ImportError:
+    CARTOPY_INSTALLED = False
 
 
 def test_preprocess():
@@ -40,3 +48,23 @@ def test_backtrack():
     numpy.testing.assert_allclose(
         expected_output["e_track"].values, output["e_track"].values
     )
+
+
+@pytest.mark.skipif(not CARTOPY_INSTALLED, reason="cartopy is not installed")
+def test_visualize():
+    runner = CliRunner()
+    result = runner.invoke(
+        cli, ["visualize", "output", "tests/test_data/test_config.yaml"]
+    )
+    assert result.exit_code == 0
+    output_path = Path("tests/tmp/output_data/figures/cumulative_sources.png")
+    assert output_path.exists()
+    expected_output = Path("tests/test_data/verify_output/cumulative_sources.png")
+    stdout = matplotlib.testing.compare.compare_images(
+        output_path, expected_output, tol=1e-4
+    )
+    if stdout:
+        raise ValueError(
+            "Output figures are different! Please check the difference in the produced"
+            " `diff` figure and verify your results."
+        )
