0649484e103510c364c76150783772b95b67412c
Ruud van der Ent <r.j.vanderent@tudelft.nl>
2023-12-18 13:46:35 +0100

    remove TODO in def forwardtrack

--- src/wam2layers/tracking/forwardtrack.py
+++ src/wam2layers/tracking/forwardtrack.py
@@ -53,7 +53,6 @@ def forwardtrack(
     # Actual tracking (note: backtracking, all fluxes change sign)
     bc = config.periodic_boundary  # boundary condition True/False
     # TODO: apply terms in successive steps instead of all at once?
-    # TODO change direction of fluxes and sources/sinks & handle region
     s_track_lower += (
         +horizontal_advection(s_track_relative_lower, fx_lower, fy_lower, bc)
         + vertical_advection(f_vert, s_track_relative_lower, s_track_relative_upper)
