a5e495a8559721f3abd116bcebcca836c1ce8bb8
Peter Kalverla <peter.kalverla@gmx.com>
2024-03-27 13:35:19 +0100

    Calculate with kgs instead of m3

--- src/wam2layers/preprocessing/shared.py
+++ src/wam2layers/preprocessing/shared.py
@@ -328,27 +328,26 @@ def stagger_y(f):
 
 
 def change_units(data):
-    """Change units to m3 or m3/s.
-    Multiply by edge length or area to get flux in m3/s
-    Divide by density of water to go from kg to m3
+    """Change units to kg or kg/s.
+
+    Multiply by edge length or area to get flux in kg/s and state in kg
     """
-    density = 1000  # [kg/m3]
     a, ly, lx = get_grid_info(data)
 
     for variable in data.data_vars:
         if variable in ["fx_upper", "fx_lower"]:
-            data[variable] *= ly / density
+            data[variable] *= ly
         elif variable in ["fy_upper", "fy_lower"]:
-            data[variable] *= lx[:, None] / density
+            data[variable] *= lx[:, None]
         elif variable in ["evap", "precip", "s_upper", "s_lower"]:
-            data[variable] *= a[:, None] / density
+            data[variable] *= a[:, None]
         else:
             raise ValueError(f"Unrecognized variable {variable}")
 
         if variable in ["s_upper", "s_lower"]:
-            data[variable] = data[variable].assign_attrs(units="m**3")
+            data[variable] = data[variable].assign_attrs(units="kg")
         else:
-            data[variable] = data[variable].assign_attrs(units="m**3 s**-1")
+            data[variable] = data[variable].assign_attrs(units="kg s-1")
 
 
 def stabilize_fluxes(F, S, progress_tracker: ProgressTracker, config: Config, t):
