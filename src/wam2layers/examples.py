57a806d66fd3eba7618fd9f5f3061bb71aa76af3
Peter Kalverla <peter.kalverla@gmx.com>
2024-04-09 15:45:25 +0200

    Implement 4TU downloader

--- src/wam2layers/examples.py
+++ src/wam2layers/examples.py
@@ -1,12 +1,55 @@
+"""Functions for preparing example cases.
+
+
+Each prepare function should:
+
+* Create a new directory in the current working directory
+* Add a configuration file (may be shipped with the repo or fetched from elsewhere)
+* Add meteorological input data (download from external repository)
+* Add a tracking_region file (may be shipped with the repo or fetched from elsewhere)
+
+Sources should be persistent (ideally with DOI)
+"""
+
+import io
+import zipfile
+from pathlib import Path
 from typing import Callable
+
+import requests
+
+
+def extract_from_4TU_archive(download_link, target_dir):
+    # See https://stackoverflow.com/a/14260592
+    response = requests.get(download_link, stream=True)  # TODO stream necessary or not?
+    assert response.ok, "Download failed"
+    z = zipfile.ZipFile(
+        io.BytesIO(response.content)
+    )  # TODO should I use context managers here?
+    z.extractall(target_dir)
 
 
 def download_volta() -> None:
-    print("downloading volta")  # TODO: implement
+    # TODO: replace with correct link; this is a random other dataset that's already published
+    download_link = "https://data.4tu.nl/ndownloader/items/4e291b8f-a37e-4378-8ca6-954a44fdc8fb/versions/2"
+    target_directory = Path.cwd() / "example_volta"
+
+    print(
+        f"Downloading and extracing example data for Volta forward tracking case to {target_directory}"
+    )
+    extract_from_4TU_archive(download_link, target_directory)
 
 
 def download_eiffel() -> None:
-    print("downloading eiffel")  # TODO: implement
+    # TODO: replace with correct link; this is a random other dataset that's already published
+    download_link = "https://data.4tu.nl/ndownloader/items/4e291b8f-a37e-4378-8ca6-954a44fdc8fb/versions/2"
+    target_directory = Path.cwd() / "example_eiffel"
+
+    print(
+        f"Downloading and extracing example data for Eiffel backward tracking case to {target_directory}"
+    )
+
+    extract_from_4TU_archive(download_link, target_directory)
 
 
 AVAILABLE_CASES: dict[str, Callable[[], None]] = {
