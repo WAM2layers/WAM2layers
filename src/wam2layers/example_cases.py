fc3f53bb01f5c64e48bd5e967fc799efac3bde49
Peter Kalverla <peter.kalverla@gmx.com>
2024-06-16 14:38:12 +0200

    Download example data (#368)
    * Add download command to CLI
    
    * Implement 4TU downloader
    
    * Add random test dataset
    
    * More semantic casenames
    
    * more tweaking
    
    * Rewrite download using 4TU API and opendap, with published DOIs
    
    * keep semantic names
    
    * Add some tests and rename examples to download
    
    * Move list with example_cases to its own file
    
    * Also download non-opendap files
    
    * unpack
    
    * suggestions from code review

---
+++ src/wam2layers/example_cases.py
@@ -0,0 +1,6 @@
+"""Example datasets that are available for automatic downloading."""
+
+AVAILABLE_CASES: dict[str, str] = {
+    "example-input-volta": "10.4121/bbe10a2a-39dc-4098-a69f-0f677d06ecdd.v2",
+    "example-input-eiffel": "10.4121/f9572240-f179-4338-9e1b-82c5598529e2.v1",
+}
