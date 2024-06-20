811a23d99c2447b878e4f853793bb1b3917b856f
chrisweij <chris.weijenborg@wur.nl>
2024-06-20 14:47:44 +0200

    Updated visualisation functions

--- src/wam2layers/utils/Visualisation_Functions.py
+++ src/wam2layers/utils/Visualisation_Functions.py
@@ -81,12 +81,11 @@ def logfile(config_file,filename,outdir):
     subset = ds_p.precip.sel(time=slice(start, end))
     precip = (subset * region * 3600).sum('time').compute()
     
-    
     #Load tracked moisture
     a_gridcell, lx, ly = get_grid_info(region)
     output_files = f"{config.output_folder}/*.nc"
     ds_e = xr.open_mfdataset(output_files, combine='nested', concat_dim='time')
-    e_track = ds_e.e_track.sum('time').compute() * 1000 / a_gridcell [:, None]
+    e_track = ds_e.e_track.sum('time').compute()
         
     #Get moisture flux data
     path_input = f"{config.preprocessed_data_folder}/*.nc"
@@ -98,13 +97,9 @@ def logfile(config_file,filename,outdir):
     
     mean_subset = ds.sum(dim='time')
        
-    
     #calculate evaporation and precipitation ratio
-       
-    mean_subset['evap_masked'] = mean_subset['evap'].where(region, drop=True) * 1000
-    
-    mean_subset['precip_masked'] = mean_subset['precip'].where(region, drop=True) * 1000
-    
+    mean_subset['evap_masked'] = mean_subset['evap'].where(region, drop=True) * 100
+    mean_subset['precip_masked'] = mean_subset['precip'].where(region, drop=True) * 1000    
     mean_subset['tracked_masked'] = e_track.where(region, drop=True)
     
     mean_subset = mean_subset.assign(e_r=(mean_subset['tracked_masked'].sum() / mean_subset['evap_masked'].sum())*100)
@@ -113,14 +108,20 @@ def logfile(config_file,filename,outdir):
     precip_sum = precip.sum()
     e_track_sum = e_track.sum()
     
-    diff = abs(precip_sum - e_track_sum)
+    precip_mean  = (precip*a_gridcell[:, None]).sum()/a_gridcell[:, None].sum() #Should only be target area
+    e_track_mean  = (e_track*a_gridcell[:, None]).sum()/a_gridcell[:, None].sum()
     
+    diff = abs(precip_sum - e_track_sum)
+    diff_areacorrected = abs((precip*a_gridcell[:, None]).sum() - (e_track*a_gridcell[:, None]).sum())
     
     #Write to file
     logfile = open(outdir+"/Logfile_"+filename+".txt",'w')
     logfile.write('precip_sum: '+str(float(precip_sum))+'\n')
     logfile.write('e_track_sum: '+str(float(e_track_sum))+'\n')
     logfile.write('abs_diff: '+str(float(diff))+'\n')
+    logfile.write('precip_mean: '+str(float(precip_mean))+'\n')
+    logfile.write('e_track_mean: '+str(float(e_track_mean))+'\n')
+    logfile.write('abs_diff_areacorrected: '+str(float(diff_areacorrected))+'\n')
     logfile.write('e_r: '+str(float(mean_subset['e_r']))+'\n')
     logfile.write('p_r: '+str(float(mean_subset['p_r']))+'\n')
     
@@ -213,7 +214,6 @@ def precipitation(config_file,filename,outdir,step, fontsizes, pads):
     bin = 50
     cmap_own = LinearSegmentedColormap.from_list(cmap_name, colors, N=bin)
     
-    
     #ax_mini = fig.add_axes([0.01, 0.01, 0.12, 0.2], projection=crs.PlateCarree(),facecolor='None') #x,y,w,h
     ax_mini = fig.add_axes([0.1, 0.1, 0.12, 0.2], projection=crs.PlateCarree(),facecolor='None') #x,y,w,h
 
@@ -235,8 +235,6 @@ def precipitation(config_file,filename,outdir,step, fontsizes, pads):
     out_dir = Path(config.output_folder) / "figures"
     out_dir.mkdir(exist_ok=True, parents=True)
     fig.savefig(outdir+"/Precipitation_input_"+filename+".png", bbox_inches='tight', dpi=200)
-
-
 
 def evaporation(config_file,filename,data_path,outdir,step, fontsizes, pads):
     """
@@ -271,7 +269,7 @@ def evaporation(config_file,filename,data_path,outdir,step, fontsizes, pads):
     end = config.tracking_end_date
     
     #Load evaporation
-    e_track = ds.e_track.sum('time').compute() #* 1000 / a_gridcell [:, None]
+    e_track = ds.e_track.sum('time').compute()
     e_track = np.where(e_track>0.1,e_track,np.nan)
         
     #Background data - isobars, wind field etc. 
@@ -304,67 +302,28 @@ def evaporation(config_file,filename,data_path,outdir,step, fontsizes, pads):
   
     all_sp = []
     for year in years:
-      for month in months:
-        ERA5_data_path = data_path+year+'/'+month+'/'
-        subs = '*sp.nc'
-        ds = xr.open_mfdataset(ERA5_data_path+subs)
-        ds = ds / 100
-        all_sp.append(ds)
-      
+        for month in months:
+            ERA5_data_path = data_path+year+'/'+month+'/'
+            subs = '*sp.nc'
+            ds = xr.open_mfdataset(ERA5_data_path+subs)
+            ds = ds / 100
+            all_sp.append(ds)
+
     ds_sp = xr.concat(all_sp, dim="time")
     mean = ds_sp.mean(dim='time')
     
     print('p loaded')
     
-    #v
-    print('now loading v')
-    all_v = []
-    for year in years:
-      for month in months:
-        ERA5_data_path = data_path+year+'/'+month+'/'
-        subs = '*ml_v.nc'
-        ds = xr.open_mfdataset(ERA5_data_path+subs)
-        level_v = ds.sel(level=115)
-        all_v.append(level_v)
-      
-    ds_v = xr.concat(all_v, dim="time")
-    mean_v = ds_v.mean(dim='time')
-    
-    print('v loaded')
-    
-    #u
-    print('now loading u')
-    all_u = []
-    for year in years:
-      for month in months:
-        ERA5_data_path = data_path+year+'/'+month+'/'
-        subs = '*ml_u.nc'
-        ds = xr.open_mfdataset(ERA5_data_path+subs)
-        level_u = ds.sel(level=115)
-        all_u.append(level_u)
-      
-    ds_u = xr.concat(all_u, dim="time")
-    mean_u = ds_u.mean(dim='time')
-    
-    print('u loaded')
-    
     #Moisture fluxes   
     path_input = f"{config.preprocessed_data_folder}/*.nc"
     ds = xr.open_mfdataset(path_input)
-    
-    #start = config.track_start_date
-    #end = config.track_end_date
     subset = ds.sel(time=slice(start, end))
       
-    mean_subset = subset.mean(dim='time')
-        
-    mean_subset = mean_subset.assign(mean_lower=(np.sqrt((mean_subset['fy_lower'])**2 + (mean_subset['fx_lower'])**2)))
-    
-    mean_subset = mean_subset.assign(mean_upper=(np.sqrt((mean_subset['fy_upper'])**2 + (mean_subset['fx_upper'])**2)))
-    
-    mean_subset = mean_subset.assign(mean_x=(mean_subset['fx_upper'] + mean_subset['fx_lower'])/2)
-    
-    mean_subset = mean_subset.assign(mean_y=(mean_subset['fy_upper'] + mean_subset['fy_lower'])/2)
+    mean_subset = subset.mean(dim='time')        
+    mean_subset = mean_subset.assign(mean_lower=(np.sqrt((mean_subset['fy_lower'])**2 + (mean_subset['fx_lower'])**2)))    
+    mean_subset = mean_subset.assign(mean_upper=(np.sqrt((mean_subset['fy_upper'])**2 + (mean_subset['fx_upper'])**2)))    
+    mean_subset = mean_subset.assign(mean_x=(mean_subset['fx_upper'] + mean_subset['fx_lower']))    
+    mean_subset = mean_subset.assign(mean_y=(mean_subset['fy_upper'] + mean_subset['fy_lower']))
     
     #Get limits
     longitude = np.array(region.longitude.values)
@@ -380,7 +339,6 @@ def evaporation(config_file,filename,data_path,outdir,step, fontsizes, pads):
     fig = plt.figure(figsize=(16, 10))
     ax = fig.add_subplot(111, projection=crs.PlateCarree())
     
-    
     #pressure
     min_p = (np.round(np.nanmean(mean['sp']))//5)*5
     max_p = (np.round(np.nanmax(mean['sp']))//5)*5
@@ -395,8 +353,7 @@ def evaporation(config_file,filename,data_path,outdir,step, fontsizes, pads):
     ax.add_feature(cfeature.RIVERS,zorder=2,linewidth=3,alpha=0.5)
     ax.add_feature(cfeature.STATES,zorder=2,facecolor='w')
     ax.add_feature(cfeature.LAKES,zorder=2,linewidth=0.8,edgecolor='k',alpha=0.5)
-    
-        
+         
     #Evaporation
     level_max = (np.round(np.nanmax(e_track))//step)*step
     level_min = 0
@@ -501,53 +458,20 @@ def moisture_fluxes(config_file,filename,data_path,outdir,step, fontsizes, pads)
     
     #Surface pressure
     print('now loading p')
-  
     
     all_sp = []
     for year in years:
-      for month in months:
-        ERA5_data_path = data_path+year+'/'+month+'/'
-        subs = '*sp.nc'
-        ds = xr.open_mfdataset(ERA5_data_path+subs)
-        ds = ds / 100
-        all_sp.append(ds)
-      
+        for month in months:
+            ERA5_data_path = data_path+year+'/'+month+'/'
+            subs = '*sp.nc'
+            ds = xr.open_mfdataset(ERA5_data_path+subs)
+            ds = ds / 100
+            all_sp.append(ds)
+
     ds_sp = xr.concat(all_sp, dim="time")
     mean = ds_sp.mean(dim='time')
     
     print('p loaded')
-    
-    #v
-    print('now loading v')
-    all_v = []
-    for year in years:
-      for month in months:
-        ERA5_data_path = data_path+year+'/'+month+'/'
-        subs = '*ml_v.nc'
-        ds = xr.open_mfdataset(ERA5_data_path+subs)
-        level_v = ds.sel(level=115)
-        all_v.append(level_v)
-      
-    ds_v = xr.concat(all_v, dim="time")
-    mean_v = ds_v.mean(dim='time')
-    
-    print('v loaded')
-    
-    #u
-    print('now loading u')
-    all_u = []
-    for year in years:
-      for month in months:
-        ERA5_data_path = data_path+year+'/'+month+'/'
-        subs = '*ml_u.nc'
-        ds = xr.open_mfdataset(ERA5_data_path+subs)
-        level_u = ds.sel(level=115)
-        all_u.append(level_u)
-      
-    ds_u = xr.concat(all_u, dim="time")
-    mean_u = ds_u.mean(dim='time')
-    
-    print('u loaded')
     
     #Moisture fluxes
     path_input = f"{config.preprocessed_data_folder}/*.nc"
@@ -557,13 +481,10 @@ def moisture_fluxes(config_file,filename,data_path,outdir,step, fontsizes, pads)
     end = config.tracking_end_date
     subset = ds.sel(time=slice(start, end))
       
-    mean_subset = subset.mean(dim='time')
-        
+    mean_subset = subset.mean(dim='time')        
     mean_subset = mean_subset.assign(mean_lower=(np.sqrt((mean_subset['fy_lower'])**2 + (mean_subset['fx_lower'])**2)))
-    
     mean_subset = mean_subset.assign(mean_upper=(np.sqrt((mean_subset['fy_upper'])**2 + (mean_subset['fx_upper'])**2)))
-       
-    
+          
     #Get limits
     longitude = np.array(region.longitude.values)
     latitude = np.array(region.latitude.values)
@@ -593,8 +514,7 @@ def moisture_fluxes(config_file,filename,data_path,outdir,step, fontsizes, pads)
     ax.add_feature(cfeature.RIVERS,zorder=2,linewidth=3,alpha=0.5)
     ax.add_feature(cfeature.STATES,zorder=2,facecolor='w')
     ax.add_feature(cfeature.LAKES,zorder=2,linewidth=0.8,edgecolor='k',alpha=0.5)
-    
-        
+      
     #Moisture flux 
     level_max = (np.round(np.nanmax(mean_subset.mean_lower))//step)*step
     level_min = 0
@@ -626,8 +546,7 @@ def moisture_fluxes(config_file,filename,data_path,outdir,step, fontsizes, pads)
     
     converted_longitudes = (longitude_sel + 180) % 360 - 180
     ax.set_xlim(np.nanmin(converted_longitudes)-50,np.nanmax(converted_longitudes)+50)
-
-        
+    
     #Grid
     gl = ax.gridlines(draw_labels=True,alpha=0.5, linestyle=':', color='k')
     gl.top_labels = False
@@ -639,7 +558,6 @@ def moisture_fluxes(config_file,filename,data_path,outdir,step, fontsizes, pads)
     out_dir = Path(config.output_folder) / "figures"
     out_dir.mkdir(exist_ok=True, parents=True)
     fig.savefig(outdir+"/Moisture_flux_lower_"+filename+".png", bbox_inches='tight', dpi=200)
-    
     
     # Make figure - upper
     fig = plt.figure(figsize=(16, 10))
@@ -706,14 +624,3 @@ def moisture_fluxes(config_file,filename,data_path,outdir,step, fontsizes, pads)
     out_dir = Path(config.output_folder) / "figures"
     out_dir.mkdir(exist_ok=True, parents=True)
     fig.savefig(outdir+"/Moisture_flux_upper_"+filename+".png", bbox_inches='tight', dpi=200)
-    
-#RUN
-config_file = '/home/WUR/weije043/virtual_envs/src/WAM2layers/Scotland_config_new.yaml'    #Location of the configuration '.yaml' file, in the file everything should be specified accordingly. 
-outdir = '/home/WUR/weije043/virtual_envs/src/WAM2layers/Visualisation'                                   #Location where the output is stored e.g. a visualistion folder. 
-filename = 'CloudRoots_Amazon'
-data_path = '/scratch/vdefeiter/ERA5/ml/'
-
-#logfile(config_file,filename,outdir)
-#precipitation(config_file,filename,outdir, step = 20, fontsizes = 20, pads = 40)
-#evaporation(config_file,filename,data_path,outdir, step = 4, fontsizes = 20, pads = 40)
-#moisture_fluxes(config_file,filename,data_path,outdir, step = 60, fontsizes = 20, pads = 40)
