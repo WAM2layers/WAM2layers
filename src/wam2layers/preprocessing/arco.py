5694f58b443a082318a9b25a9a477ed8d77be3d2
Bart Schilperoort <b.schilperoort@esciencecenter.nl>
2024-12-06 15:09:50 +0100

    Use CFtime, cache zarr opening

--- src/wam2layers/preprocessing/arco.py
+++ src/wam2layers/preprocessing/arco.py
@@ -1,14 +1,38 @@
 """Load ARCO-ERA5: preprocessing from zarr."""
 
-from time import time
+import datetime as pydt
+from functools import cache
 
 import numpy as np
-import pandas as pd
 import xarray as xr
 
+from wam2layers import __version__
 from wam2layers.config import Config
 from wam2layers.preprocessing.cmip import LATENT_HEAT
 from wam2layers.preprocessing.utils import calculate_humidity
+from wam2layers.utils.calendar import CfDateTime
+
+
+PREPROCESS_ATTRS = {
+    "title": "ARCO-ERA5 data preprocessed for use in WAM2layers",
+    "history": (
+        f"created on {pydt.datetime.now(pydt.timezone.utc):%Y-%m-%dT%H:%M:%SZ} "
+        f"using wam2layers version {__version__}."
+    ),
+    "source": (
+        "ARCO-ERA5: An Analysis-Ready Cloud-Optimized Reanalysis Dataset, "
+        "https://github.com/google-research/arco-era5, "
+        "ECMWF Reanalysis v5 (ERA5), "
+        "www.ecmwf.int/en/forecasts/dataset/ecmwf-reanalysis-v5"
+    ),
+    "references": (
+        "doi.org/10.5281/zenodo.7010594, "
+        "doi.org/10.5194/esd-5-471-2014, "
+        "https://ams.confex.com/ams/103ANNUAL/meetingapp.cgi/Paper/415842"
+    ),
+    "Conventions": "CF-1.6",
+}
+
 
 REQUIRED_VARS = [
     "u_component_of_wind",
@@ -35,30 +59,24 @@ ARCO2WAM = {
 }
 
 
-def get_input_data(datetime: pd.Timestamp, config: Config):
+def get_input_data(datetime: CfDateTime, config: Config):
     """Get the preprocessing input data for ARCO-ERA5."""
-    t0 = time()
-    assert config.level_type == "pressure_levels"
+    if config.level_type != "pressure_levels":
+        msg = "Currently the ARCO-ERA5 preprocessor only supports pressure level data."
+        raise ValueError(msg)
+    
+    if config.calendar != "proleptic_gregorian":
+        msg = (
+            "ARCO-ERA5 data is on a 'proleptic_georgian' calendar.\n"
+            "Please set this calendar in your configuration file."
+        )
+        raise ValueError(msg)
 
-    data = xr.open_zarr(
-        "gs://gcp-public-data-arco-era5/ar/full_37-1h-0p25deg-chunk-1.zarr-v3",
-        chunks=None,
-        storage_options=dict(token="anon"),
-    )
+    ds = open_arco_pl()
+    data = ds.sel(time=datetime).compute()
 
-    data = data.sel(time=datetime, method="nearest")
-
-    ## Note: it is possible to reduce the domain, but due to the chunking (all lats/lons
-    ## are in a single chunk, it won't save you any data transfer time). If storage
-    ## allows: do the whole world.
-    # if config.tracking_domain is not None:  # Reduce spatial domain
-    #     data=data.sel(
-    #         latitude=slice(config.tracking_domain.north, config.tracking_domain.south),
-    #         longitude=slice(config.tracking_domain.west, config.tracking_domain.east),
-    #     )
     data = data.sel(level=config.levels)  # Only select wanted levels
 
-    data = data[REQUIRED_VARS]
     data["qs"] = calculate_humidity(
         data["2m_dewpoint_temperature"], data["surface_pressure"]
     )
@@ -80,5 +98,19 @@ def get_input_data(datetime: pd.Timestamp, config: Config):
         ]
     )
     data.load()  # Load ARCO data into memory
-    print(f"Elapsed time ARCO load: {time() - t0:.1f} s")
-    return data, {}
+    return data, PREPROCESS_ATTRS
+
+
+@cache
+def open_arco_pl():
+    """Open the required variables from ARCO in an efficient manner."""
+    data = xr.open_zarr(
+        "gs://gcp-public-data-arco-era5/ar/full_37-1h-0p25deg-chunk-1.zarr-v3",
+        chunks=None,
+        use_cftime=True,
+        storage_options={"token": "anon"},
+    )[REQUIRED_VARS]
+
+    return data.sel(
+        time=slice(data.attrs["valid_time_start"], data.attrs["valid_time_stop"])
+    )
